fixed pixel averaging

pixel.py
@@ -34,11 +34,10 @@ def average_square(pixels):
     green = pixels[:, :, 1]
     blue = pixels[:, :, 2]
 
-    avg_r = sum(sum(red))/red.shape[0]**2
-    avg_g = sum(sum(green))/green.shape[0]**2
-    avg_b = sum(sum(blue))/blue.shape[0]**2
-
-    # print(pixels)
+    num_pix = red.shape[0]**2
+    avg_r = np.sum(red)/num_pix
+    avg_g = np.sum(green)/num_pix
+    avg_b = np.sum(blue)/num_pix
 
     return [avg_r, avg_g, avg_b]
 
@@ -54,10 +53,7 @@ if __name__ == "__main__":
     squaresize = 25  # side length of superpixel
     numcols = math.floor(width/squaresize)
     numrows = math.floor(height/squaresize)
-    super_square1 = get_square(x, 1, 1, squaresize)
+    super_square1 = get_square(x, 18, 18, squaresize)
 
     superpixel1 = average_square(super_square1)
-
-    plt.imshow(superpixel1)
-    plt.show()
     print(average_square(super_square1))