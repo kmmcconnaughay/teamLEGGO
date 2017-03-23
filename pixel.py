8b0d5fa047d00fca68f368eb9f7fcd008d7f22c3
anilpatel38 <anil.patel@students.olin.edu>
2017-03-23 15:04:47 -0400

    pixelizing works! displaying it properly, not so much.

--- pixel.py
+++ pixel.py
@@ -10,6 +10,9 @@ import PIL.Image as Image
 
 
 def load_img(filename):
+    """
+    load an image from a file and return it as an array of rgb values.
+    """
     img = Image.open(filename, 'r')
     data = np.array(img)
     return data
@@ -20,21 +23,32 @@ def get_square(array, row, col, size):
     gets all of the pixels of a supersquare in the array for a given row,
     column, and size for each superpixel.
     """
+
+    # figure out where in the full image array the pixels are
     firstcol = col*size
     firstrow = row*size
     lastcol = firstcol+size
     lastrow = firstrow+size
 
+    # return only those pixels we've determined to be in the desired superpixel
     pixels = array[firstcol:lastcol, firstrow:lastrow, :]
     return pixels
 
 
 def average_square(pixels):
+    """
+    Take in a list of pixels and return the average rgb values of the list
+    """
+
+    # unpack all values of each color from all pixels
     red = pixels[:, :, 0]
     green = pixels[:, :, 1]
     blue = pixels[:, :, 2]
 
+    # calculate number of pixels being input
     num_pix = red.shape[0]**2
+
+    # sum color values and divide to get the average value
     avg_r = np.sum(red)/num_pix
     avg_g = np.sum(green)/num_pix
     avg_b = np.sum(blue)/num_pix
@@ -42,33 +56,68 @@ def average_square(pixels):
     return [avg_r, avg_g, avg_b]
 
 
-def get_pixel(super_square):
-    values = average_square(super_square)
+def get_pixel(super_pixel):
+    """
+    creates a superpixel matrix consisting of all the average rgb values
+    """
+
+    values = average_square(super_pixel)   # grab average rgb values
+    pix = super_pixel.shape[0]             # determine pixel side length
+    size = (pix, pix)                       # make a tuple of superpixel size
+
+    # unpack rgb averages
     red_val = values[0]
     green_val = values[1]
     blue_val = values[2]
 
-    red_matrix = red_val * np.ones((25, 25))
-    green_matrix = green_val * np.ones((25, 25))
-    blue_matrix = blue_val * np.ones((25, 25))
+    # create matrices of superpixel size with all average values
+    red_matrix = red_val * np.ones(size)
+    green_matrix = green_val * np.ones(size)
+    blue_matrix = blue_val * np.ones(size)
+
+    # stack matrices on top of each other
     final = np.dstack((red_matrix, green_matrix, blue_matrix))
     return final
 
 
+def pixelate_dat_ish(file_name, pixel_size):
+    """
+    combine all functions into a fully pixelated image
+    """
+
+    array = load_img(file_name)     # create the image array
+    size = array.shape              # determine size of the pic
+    height = size[0]                # height of the pic
+    width = size[1]                 # width of the pic
+
+    squaresize = pixel_size                    # side length of superpixel
+    numcols = math.floor(width/squaresize)     # number of columns rounded down
+    numrows = math.floor(height/squaresize)    # number of rows rounded down
+
+    # create an empty array to add to
+    pixelated = np.empty((height, width, 3))
+
+    for col in range(0, numcols):
+        for row in range(0, numrows):
+            pixels = get_square(array, col, row, squaresize)
+            super_pixel = get_pixel(pixels)
+
+            firstcol = col*pixel_size
+            lastcol = firstcol+pixel_size
+
+            firstrow = row*pixel_size
+            lastrow = firstrow+pixel_size
+            pixelated[firstrow:lastrow, firstcol:lastcol, :] = super_pixel
+
+    return pixelated
+
+
 if __name__ == "__main__":
+
     x = load_img('wallaby.jpg')
-    # plt.imshow(x)
-    # plt.show()
+    plt.imshow(x)
+    plt.show()
 
-    size = x.shape
-    width = size[1]
-    height = size[0]
-    squaresize = 25  # side length of superpixel
-    numcols = math.floor(width/squaresize)
-    numrows = math.floor(height/squaresize)
-    super_square1 = get_square(x, 1, 1, squaresize)
-
-    superpixel1 = average_square(super_square1)
-    print(average_square(super_square1))
-    plt.imshow(get_pixel(super_square1))
+    image_pix = pixelate_dat_ish('wallaby.jpg', 25)
+    plt.imshow(image_pix)
     plt.show()
