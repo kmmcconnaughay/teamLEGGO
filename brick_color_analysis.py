cf3c8d705035b354751fe1de4d4840615b98e9c3
Kerry McConnaughay <kmmcconnaughay@gmail.com>
2017-03-30 14:39:53 -0400

    Working brick color analysis!

--- brick_color_analysis.py
+++ brick_color_analysis.py
@@ -8,47 +8,35 @@ Author: Kerry McConnaughay
 import cv2
 import PIL.Image as Image
 import numpy as np
-from skimage import data
-d = {}
 
 def load_image(filenames):
     """
     Load a brick image from the directory and return image size.
     """
-    dimensions_images = []
-    image_arrays = []
+    brick_pixels = {}
+    image_middle_pixels = []
     path = '/home/kerry/teamLEGGO/Pick A Brick_LEGO_All_Bricks'
     for filename in filenames:
         image = Image.open(path + '//' + filename, 'r')
+        # image = Image.open(path + '//Black', 'r')
         image.load()
         width, height = image.size
-        dimensions = width, height
-        dimensions_images.append(dimensions)
+        # print(width)
+        # print(height)
 
         data = np.array(image)
-        image_arrays.append(data)
-    print(dimensions_images)
-    print(image_arrays)
-    return image_arrays, dimensions_images
-"""
+        middle_pixel = data[int(height/2), int(0.6 * width)]
+        brick_pixels[filename] = middle_pixel
+        image_middle_pixels.append(middle_pixel)
+        # print(filename, middle_pixel)
+        # print(middle_pixel)
+    return brick_pixels, image_middle_pixels
 
-# AVERAGE COLOR VALUE OF EACH ROW OF pixels
-# average_color_per_row = numpy.average(image, axis = 0)
-# MAKE IMAGE OUT OF AVERAGE COLOR value
-# average_color_img = numpy.array([[average_color]*100]*100, numpy.uint8)
-# cv2.imwrite( "average_color.png", average_color_img )
-
-
-def save_image(npdata, out_filename):
-    image = Image.fromarray(np.array( ycc_uint8, 'RGB')
-    image.save(out_filename)
-"""
 
 if __name__ == '__main__':
-    filenames = ['Black', 'Brick yellow', 'Bright blue', 'Bright orange', 'Bright red',
+    filenames = ['Brick yellow', 'Bright blue', 'Bright orange', 'Bright red',
     'Bright reddish violet', 'Bright yellow', 'Bright yellowish green', 'Dark green',
     'Dark stone grey', 'Light purple', 'Medium azur', 'Medium blue', 'Medium lavendel',
-    'Medium lilac', 'Medium nougat', 'Medium stone grey', 'Reddish brown', 'Sand green',
-    'White']
-    # load_image_pixels()
+    'Medium lilac', 'Medium nougat', 'Medium stone grey', 'Reddish brown', 'Sand green'
+    ]
     load_image(filenames)
