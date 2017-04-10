4224b636de71055a957a46a51f0e698b2a9c740a
otalu <onurtalu@gmail.com>
2017-04-10 14:00:33 -0400

    Added Kerry's code. This is temporary

---
+++ Pixelation/brick_color_analysis.py
@@ -0,0 +1,41 @@
+"""
+Take a saved brick image, get RGB value, save the RGB and file name as tuples in
+a dictionary, and print the dictionary.
+
+Author: Kerry McConnaughay
+
+"""
+import cv2
+import PIL.Image as Image
+import numpy as np
+
+
+def load_image(filenames):
+    """
+    Load a brick image from the directory and return image size.
+    """
+    brick_pixels = {}
+    image_middle_pixels = []
+    path = '/home/onurtalu/Documents/teamLEGGO/Pick A Brick_LEGO_All_Bricks'
+    for filename in filenames:
+        image = Image.open(path + '//' + filename, 'r')
+        image.load()
+        width, height = image.size
+
+        data = np.array(image)
+        middle_pixel = data[int(height/2), int(0.6 * width)]
+        brick_pixels[filename] = middle_pixel
+        image_middle_pixels.append(middle_pixel)
+        # print(filename, middle_pixel)
+        # print(middle_pixel)
+    return brick_pixels, image_middle_pixels
+
+
+if __name__ == '__main__':
+    filenames = ['Brick yellow', 'Bright blue', 'Bright orange', 'Bright red',
+                 'Bright reddish violet', 'Bright yellow',
+                 'Bright yellowish green', 'Dark green', 'Dark stone grey',
+                 'Light purple', 'Medium azur', 'Medium blue',
+                 'Medium lavendel', 'Medium lilac', 'Medium nougat',
+                 'Medium stone grey', 'Reddish brown', 'Sand green']
+    load_image(filenames)
