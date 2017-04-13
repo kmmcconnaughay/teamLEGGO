97f74e15777a56324463e6b5818f486ec93bcbef
askrishnan <anupamakrishnan@students.olin.edu>
2017-04-13 14:03:26 -0400

    File selecting and stuff

---
+++ UI.py
@@ -0,0 +1,90 @@
+from kivy.app import App
+from kivy.garden.filebrowser import FileBrowser
+from kivy.utils import platform
+from kivy.app import App
+from os.path import sep, expanduser, dirname
+from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.logger import Logger
+from kivy.uix.scatter import Scatter
+from glob import glob
+import os
+import shutil
+
+
+class DisplayImg(App):
+    def build(self):
+        root = self.root
+        curdir = dirname(SelectFile.file_name)
+        for file_name in glob(join(curdir, 'images', '*')):
+            try:
+                picture = Picture(source-file_name, rotation=randint(-30, 30))
+                root.add_widget(picture)
+            except Exception as e:
+                Logger.exception('Pictures: Unable to load <%s>' % file_name)
+
+    def on_pause(self):
+        return True
+
+
+class PngPls(Popup):
+
+    def popup(self):
+        text = "Please input a .png file."
+        content = BoxLayout(orientation="vertical")
+        content.add_widget(Label(text=text, font_size=20))
+        mybutton = Button(text="Ok!", size_hint=(1, .20), font_size=20)
+        content.add_widget(mybutton)
+
+        pngpopup = Popup(content=content,
+                         title="C'mon",
+                         auto_dismiss=False,
+                         size_hint=(.7, .5))
+        mybutton.bind(on_press=pngpopup.dismiss)
+        pngpopup.open()
+
+
+class SelectFile(App):
+
+    def build(self):
+        if platform == 'win':
+            user_path = dirname(expanduser('~')) + sep + 'Documents'
+        else:
+            user_path = expanduser('~') + sep + 'Documents'
+        browser = FileBrowser(select_string='Select',
+                              favorites=[(user_path, 'Documents')])
+        browser.bind(
+                    on_success=self._fbrowser_success,
+                    on_canceled=self._fbrowser_canceled)
+        return browser
+
+    def _fbrowser_canceled(self, instance):
+        SelectFile.stop()
+
+    def _fbrowser_success(self, instance):
+        self.selected_image = str(instance.selection)
+        i = len(instance.selection)
+        self.selected_image = self.selected_image[2:i-3]
+        self.file_name = self.selected_image.rsplit('/', 1)[-1]
+        n = len(self.file_name)
+        if self.file_name[n-4:n] == ".png":
+            cwd = os.getcwd()
+            shutil.move(self.selected_image, cwd + self.file_name)
+            return Image(source=self.file_name)
+            self.stop()
+        else:
+            pngpls = PngPls()
+            return pngpls.popup()
+
+
+class MyApp(App):
+    def build(self):
+        return SelectFile().run()
+
+
+if __name__ == '__main__':
+    MyApp().run()
