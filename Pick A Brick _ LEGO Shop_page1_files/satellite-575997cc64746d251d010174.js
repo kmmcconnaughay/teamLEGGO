fbea16c2dff532ff55994c1b083f09c47c79d8ba
Annie Kroo <annie.kroo@students.olin.edu>
2017-04-27 14:40:53 -0400

    Cleaned up repo

--- Pick A Brick _ LEGO Shop_page1_files/satellite-575997cc64746d251d010174.js
+++
@@ -1,9 +0,0 @@
-_satellite.pushAsyncScript(function(event, target, $variables){
-  setLocale(event.detail.locale);
-setExchangeRate(event.detail.currencyConversionRate );
-//_satellite.setVar('environment', event.detail.env);
-_satellite.setVar('isTrackmanProd', isProduction());
-_satellite.setVar('UAID', getCookie('UAID'));
-_satellite.setVar('surveyPageCount', 0);
-_satellite.setVar('surveyPageCountLimit', 3);
-});
